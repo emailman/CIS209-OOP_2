56c8534ff6eff3b97fb98b178cab1c049ff4950b
Eric Mailman <emailman@dtcc.edu>
2018-10-05 14:49:31 -0400

    Initial version

---
+++ OOP-2/Account2.cs
@@ -0,0 +1,29 @@
+﻿namespace OOP_2
+{
+    class Account2
+    {
+        public string Name { get; }
+        public decimal Balance { get; private set; } 
+
+        public Account2(string acctName, decimal acctBalance = 0)
+        {
+            Name = acctName;
+            Balance = acctBalance;
+        }
+
+        public void Deposit(decimal amt)
+        {
+            Balance += amt;
+        }
+
+        public void Withdraw(decimal amt)
+        {
+            Balance -= amt;
+        }
+
+        override public string ToString()
+        {
+            return $"Account: {Name} / Balance = {Balance:C}";
+        }
+    }
+}
--- OOP-2/Program.cs
+++ OOP-2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOP_2
 {
@@ -10,6 +7,47 @@ namespace OOP_2
     {
         static void Main(string[] args)
         {
+            // Create a list of accounts
+            var accounts = new List<Account2>
+            {
+                new Account2("Dopey", 500),
+                new Account2("Sleepy"),
+                new Account2("Sneezy", 300)
+            };
+
+            Reports(accounts, "Opening Report");
+
+            // Do some transactions
+            accounts[0].Withdraw(150);
+            Console.WriteLine($"{accounts[0].Name}: Withdraw $150");
+
+            accounts[1].Deposit(100);
+            Console.WriteLine($"{accounts[1].Name}: Deposit $100");
+
+            accounts[2].Deposit(75);
+            Console.WriteLine($"{accounts[2].Name}: Deposit $75");
+
+            Reports(accounts, "\nClosing Report");
+        }
+
+        static void Reports(List<Account2> accts, string heading)
+        {
+            Console.WriteLine(heading);
+            Console.WriteLine("--------------");
+            Console.WriteLine("List of accounts and balances:");
+            foreach (var acct in accts)
+                Console.WriteLine(acct);
+            Console.WriteLine();
+
+            Console.WriteLine("List of account names:");
+            foreach (var acct in accts)
+                Console.WriteLine(acct.Name);
+
+            Console.Write("\nTotal Balance = ");
+            decimal balance = 0;
+            foreach (var acct in accts)
+                balance += acct.Balance;
+            Console.WriteLine($"{balance:C}\n");
         }
     }
 }
